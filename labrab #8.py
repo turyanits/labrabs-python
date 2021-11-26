a0bd42025ea8628784fa4e5799bbe443c32a8ace
turyanits <94446813+turyanits@users.noreply.github.com>
2021-11-26 19:24:15 +0200

    Add files via upload

---
+++ labrab #8.py
@@ -0,0 +1,13 @@
+# Завдання 1. #7
+"""x = float(input('x:'))
+y = float(input('y:'))
+z = float(input('z:'))
+u = (max(x, z) + max(x+y, x*y))/max((x+y, x*y))**2
+print(u)"""
+
+
+# Завдання 3. #7
+
+
+x_0 = 0
+x_1 = x_2 = 9
