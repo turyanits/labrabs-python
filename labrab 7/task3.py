af4fe4ec54f19ba7f5673a5df2f415033ec27b80
Turyanitsa Artem <94446813+turyanits@users.noreply.github.com>
2022-01-18 14:54:32 +0200

    Add files via upload

--- labrab 7/task3.py
+++ labrab 7/task3.py
@@ -6,4 +6,6 @@ m = [[randint(-10, 10) for x in range(j)] for y in range(i)]
 m = np.array(m)
 a = float(input('Введите число, на которое будет умножаться матрица:'))
 prod = a * m
+print(m)
+print('/ / / / / / / / / / / / / / /')
 print(prod)
