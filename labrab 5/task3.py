f43921c912e582d88e8dffabff6cf86a31c71342
Turyanitsa Artem <94446813+turyanits@users.noreply.github.com>
2022-01-18 14:06:09 +0200

    Add files via upload

---
+++ labrab 5/task3.py
@@ -0,0 +1,18 @@
+import math
+
+x = float(input("Ведіть х , -pi < x < pi: "))
+eps = float(input("Введіть точність eps= "))
+sum += 0
+i = 1
+d = math.sin(x)
+while (abs(d) > eps) & (i <= 10000):
+    sum += d
+    d = (-1)**i * math.sin((i+1)*x)/(i+1)
+    i += 1
+else:
+    sum *= 2
+print(sum)
+if abs(x-sum) < eps:
+   print('Тотожність виконується')
+else:
+   print('Тотожність не виконується')
